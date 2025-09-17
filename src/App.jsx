f8175805dce7c2116b194af40abffa44eaea4c2d
jaypal <katarejaypal45@gmail.com>
2025-09-17 19:02:33 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React j k</h1>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
